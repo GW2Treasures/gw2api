fcc0c1d7686cf03ee0c8d0a7604a2771af0986e6
darthmaim <darth.maim@arcor.de>
2015-05-23 13:24:22 +0200

    remove unnecessary $options parameter from PaginatedEndpointStub constructor

--- tests/Stubs/PaginatedEndpointStub.php
+++ tests/Stubs/PaginatedEndpointStub.php
@@ -9,8 +9,8 @@ use GW2Treasures\GW2Api\V2\Pagination\PaginatedEndpoint;
 class PaginatedEndpointStub extends EndpointStub implements IPaginatedEndpoint {
     use PaginatedEndpoint;
 
-    public function __construct(  GW2Api $api, $maxPageSize = 10, array $options = [ ] ) {
-        parent::__construct( $api, $options );
+    public function __construct(  GW2Api $api, $maxPageSize = 10 ) {
+        parent::__construct( $api );
 
         $this->maxPageSize = $maxPageSize;
     }
