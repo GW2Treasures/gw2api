437225592ba854a143f5292da19b7976ad5d2582
darthmaim <darth.maim@arcor.de>
2015-05-26 22:48:54 +0200

    test ApiException

---
+++ tests/ApiExceptionTest.php
@@ -0,0 +1,47 @@
+<?php
+
+use GuzzleHttp\Message\Response;
+use GuzzleHttp\Message\ResponseInterface;
+use GuzzleHttp\Stream\Stream;
+use GW2Treasures\GW2Api\V2\ApiHandler;
+use GW2Treasures\GW2Api\V2\IEndpoint;
+use Stubs\EndpointStub;
+
+class ApiExceptionTest extends TestCase {
+    protected function getEndpoint() {
+        return new EndpointStub( $this->api() );
+    }
+
+
+    /**
+     * @expectedException \GW2Treasures\GW2Api\Exception\ApiException
+     * @expectedExceptionMessage this is the error message.
+     */
+    public function testMessage() {
+        $this->mockResponse( new Response(
+            400, [ 'Content-Type' => 'application/json; charset=utf-8' ],
+            Stream::factory( '{"text":"this is the error message."}' )
+        ));
+
+        $this->getEndpoint()->test();
+    }
+
+    public function testResponse() {
+        $this->mockResponse( new Response(
+            400, [ 'Content-Type' => 'application/json; charset=utf-8', 'foo' => 'bar' ],
+            Stream::factory( '{"text":"this is the error message."}' )
+        ));
+
+        try {
+            $this->getEndpoint()->test();
+        } catch( \GW2Treasures\GW2Api\Exception\ApiException $exception ) {
+            $this->assertNotNull( $exception->getResponse() );
+            $this->assertEquals( 'bar', $exception->getResponse()->getHeader('foo') );
+            $this->assertEquals( 400, $exception->getCode() );
+            $this->assertEquals( 400, $exception->getResponse()->getStatusCode() );
+
+            $this->assertStringStartsWith( $exception->getMessage(), $exception->__toString() );
+            $this->assertNotFalse( strstr( $exception->__toString(), 'status: 400' ));
+        }
+    }
+}
