299e23ada43179c7c03cc7ada9e905ba67b4e2c0
darthmaim <darth.maim@arcor.de>
2015-07-11 17:19:22 +0200

    add test for deprecated AccountEndpoint::info() method

--- tests/V2/AccountEndpointTest.php
+++ tests/V2/AccountEndpointTest.php
@@ -1,7 +1,7 @@
 <?php
 
 class AccountEndpointTest extends TestCase {
-    public function test() {
+    public function testGet() {
         $endpoint = $this->api()->account('api_key');
 
         $this->assertEndpointIsAuthenticated( $endpoint );
@@ -17,5 +17,23 @@ class AccountEndpointTest extends TestCase {
             ]
         }');
         $this->assertEquals( 'Lawton.1234', $endpoint->get()->name );
+    }
+
+    public function testInfo() {
+        $endpoint = $this->api()->account('api_key');
+
+        $this->assertEndpointIsAuthenticated( $endpoint );
+
+        $this->mockResponse('{
+            "id":"account-guid",
+            "name"   : "Lawton.1234",
+            "world"  : 1007,
+            "guilds" : [
+                "DA9137CD-3A86-E411-B57A-00224D566B58",
+                "1F5F70AA-1DB6-E411-A2C4-00224D566B58",
+                "8B211747-3B86-E411-B57A-00224D566B58"
+            ]
+        }');
+        $this->assertEquals( 'Lawton.1234', $endpoint->info()->name );
     }
 }
