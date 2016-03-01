da9b3211f5fd25f3b59af06823d293ed81ffab56
darthmaim <darth.maim@arcor.de>
2016-03-01 19:21:28 +0100

    merge pvp endpoint tests

--- tests/V2/PvpEndpointTest.php
+++ tests/V2/PvpEndpointTest.php
@@ -4,7 +4,7 @@ use GW2Treasures\GW2Api\V2\Endpoint\Pvp\GameEndpoint;
 use GW2Treasures\GW2Api\V2\Endpoint\Pvp\StatsEndpoint;
 
 class PvpEndpointTest extends TestCase {
-    public function test() {
+    public function testPvp() {
         $endpoint = $this->api()->pvp('API_KEY');
 
         $this->assertEndpointUrl( 'v2/pvp', $endpoint );
@@ -12,5 +12,26 @@ class PvpEndpointTest extends TestCase {
 
         $this->assertInstanceOf( GameEndpoint::class, $endpoint->games() );
         $this->assertInstanceOf( StatsEndpoint::class, $endpoint->stats() );
+    }
+
+    public function testGames() {
+        $endpoint = $this->api()->pvp('API_KEY')->games();
+
+        $this->assertEndpointUrl( 'v2/pvp/games', $endpoint );
+        $this->assertEndpointIsAuthenticated( $endpoint );
+        $this->assertEndpointIsBulk( $endpoint );
+
+        $this->mockResponse( '["A9F9FD97-F114-4F97-B2CA-5E814DF0340E","4FDC931F-677F-4369-B20A-9FBB6A63B2B4"]' );
+        $this->assertContains( '4FDC931F-677F-4369-B20A-9FBB6A63B2B4', $endpoint->ids() );
+    }
+
+    public function testStats() {
+        $endpoint = $this->api()->pvp('API_KEY')->stats();
+
+        $this->assertEndpointUrl( 'v2/pvp/stats', $endpoint );
+        $this->assertEndpointIsAuthenticated( $endpoint );
+
+        $this->mockResponse( '{"pvp_rank":57}' );
+        $this->assertEquals( 57, $endpoint->get()->pvp_rank );
     }
 }
